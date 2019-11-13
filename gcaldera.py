0d6bdc2a12e458aef261d9e490e7f51c5554b050
Juana <juanamtz@gmail.com>
2019-11-13 04:23:42 -0600

    Add files via upload

---
+++ gcaldera.py
@@ -0,0 +1,28 @@
+# myplot.py
+from bokeh.plotting import figure, curdoc
+from bokeh.driving import linear
+import random
+
+p = figure(plot_width=900, plot_height=400)
+p.x_range.follow="end"
+p.x_range.follow_interval = 20
+p.x_range.range_padding=0
+r1 = p.line([], [], color="firebrick", line_width=2)
+r2 = p.line([], [], color="navy", line_width=2)
+
+ds1 = r1.data_source
+ds2 = r2.data_source
+
+@linear()
+def update(step):
+    ds1.data['x'].append(step)
+    ds1.data['y'].append(random.randint(0,100))
+    ds2.data['x'].append(step)
+    ds2.data['y'].append(random.randint(0,100))  
+    ds1.trigger('data', ds1.data, ds1.data)
+    ds2.trigger('data', ds2.data, ds2.data)
+
+curdoc().add_root(p)
+
+# Add a periodic callback to be run every 500 milliseconds
+curdoc().add_periodic_callback(update, 400)
